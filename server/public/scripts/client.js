cf6efcdb075b6e3f5c7a51a5823f8246402ecf97
david krueger <dk12181@gmail.com>
2017-09-16 10:24:54 -0500

    welcome message on the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,22 +4,8 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('displayController', function(){
 //this code inside runs when controller is initialized
-var display = this;
+var dc = this;
 // name.message = this;
-console.log(this);
-display.name = 'Enter name below';
-display.message = 'Enter message below';
 
-display.people = [];
-display.addPerson = function(nameBox, messageBox) {
-  var person = {nameBox : nameBox, messageBox : messageBox};
-  display.people.push(person);
-  console.log(display.people);
-};
-
-//create a display.delete function
-display.delete = function(spot) {
-  display.people.splice(spot, 1);
-};
 
 });//end of myApp.controller
