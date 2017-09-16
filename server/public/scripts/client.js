c848022ba092449dc1d6259ee8557e562faba66b
david krueger <dk12181@gmail.com>
2017-09-16 15:26:29 -0500

    landing page format

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,7 +5,6 @@ var myApp = angular.module('myApp', []);
 myApp.controller('displayController', function(){
 //this code inside runs when controller is initialized
 var dc = this;
-// name.message = this;
 
 
 });//end of myApp.controller
